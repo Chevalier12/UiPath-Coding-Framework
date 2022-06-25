6c85a14c884297223b4b6b52e36bdae8c05a1996
Chevalier12 <49100023+Chevalier12@users.noreply.github.com>
2022-06-25 16:07:29 +0300

    New Job_GetValue
    Added Job_GetValue (UiPath.Core.Activities.GetValue)

---
+++ UiPath.Core.CodeActivities/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Inscriber;
+
+namespace UiPath.Core.CodeActivities
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var getValue = new Job_GetValue().Run
+            (
+                WaitForReady.COMPLETE,
+                "<wnd app='notepad.exe' cls='Notepad' title='*Library.txt - Notepad' /><wnd aaname='Horizontal' cls='Edit' /><ctrl name='Text Editor' role='editable text' />"
+            );
+
+          
+        }
+    }
+}
---
+++ UiPath.Core.CodeActivities/UiPathCodeWorks.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using UiPath.Core;
+using UiPath.Core.Activities;
+
+namespace Inscriber
+{
+    public class Job_GetValue : Activity
+    {
+        public IDictionary<string, object> input = new Dictionary<string, object>();
+        public InArgument<string> SelectorArgument { get; set; }
+        public OutArgument<string> ValueArgument { get; set; }
+        public InArgument<WaitForReady> WaitForReadyArgument { get; set; }
+        public InArgument<int> TimeoutArgument { get; set; }
+        public InArgument<bool> ContineOnErrorArgument { get; set; }
+
+        public IDictionary<string, object> Run(
+            WaitForReady waitForReady = WaitForReady.NONE,
+            string Selector = null,
+            UiElement uiElement = null,
+            int TimeOut = 30,
+            bool ContinueOnError = false
+        )
+        {
+            WaitForReadyArgument = waitForReady;
+            SelectorArgument = Selector;
+            TimeoutArgument = TimeOut;
+            ValueArgument = new OutArgument<string>();
+            ContineOnErrorArgument = ContinueOnError;
+
+
+            if (Selector == null && uiElement != null)
+                SelectorArgument = uiElement.Selector.ToString();
+            else if (Selector == null && uiElement == null)
+                throw new SystemException("No Selector or uiElement parameter provided.");
+
+            Implementation = () => new Sequence
+            {
+                Activities =
+                    {
+                        new GetValue
+                        {
+                            Target = new Target
+                            {
+                                WaitForReady = new InArgument<WaitForReady>(activityContext =>
+                                    WaitForReadyArgument.Get(activityContext)),
+                                Selector = new InArgument<string>(activityContext =>
+                                    SelectorArgument.Get(activityContext)),
+                                TimeoutMS = new InArgument<int>(activityContext =>
+                                    TimeoutArgument.Get(activityContext))
+                            },
+                            ContinueOnError = new InArgument<bool>(activityContext =>
+                                ContineOnErrorArgument.Get(activityContext)),
+                            Value = new OutArgument<string>(activityContext =>
+                                ValueArgument.Get(activityContext))
+                        }
+                    }
+            };
+
+            input.Add(nameof(WaitForReadyArgument), waitForReady);
+            input.Add(nameof(SelectorArgument), Selector);
+            input.Add(nameof(TimeoutArgument), TimeOut);
+            input.Add(nameof(ContineOnErrorArgument), ContinueOnError);
+
+            IDictionary<string, object> output = new Dictionary<string, object>();
+            try
+            {
+                output = WorkflowInvoker.Invoke(this, input);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to continue or to exit...");
+                Console.ReadLine();
+            }
+
+            return output;
+        }
+    }
+}
